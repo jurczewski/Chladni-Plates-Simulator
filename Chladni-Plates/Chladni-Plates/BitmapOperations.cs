fd6348da5a8fb3f87649fd90abebe70e87f44d2d
Bartosz Jurczewski <bartek.jurczewski@gmail.com>
2019-10-27 15:30:17 +0100

    Bitmap operations moved to separate class, Physics class added

---
+++ Chladni-Plates/Chladni-Plates/BitmapOperations.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Media.Imaging;
+
+namespace Chladni_Plates
+{
+    public static class BitmapOperations
+    {
+        public static Bitmap RandomColorPixelBitmap(int width, int height)
+        {
+            var bitmap = new Bitmap(width, height);
+            var random = new Random();
+
+            //create random pixels
+            for (var y = 0; y < height; y++)
+            {
+                for (var x = 0; x < width; x++)
+                {
+                    //generate random ARGB
+                    var a = random.Next(256);
+                    var r = random.Next(256);
+                    var g = random.Next(256);
+                    var b = random.Next(256);
+
+                    //set
+                    bitmap.SetPixel(x, y, Color.FromArgb(a, r, g, b));
+                }
+            }
+
+            return bitmap;
+        }
+
+        public static Bitmap RandomPixelsBitmap(int width, int height, int numberOfParticles)
+        {
+            var bitmap = new Bitmap(width, height);
+            FillBitmapWithColor(Color.LightGray, ref bitmap);
+
+            var random = new Random();
+
+            for (int i = 0; i < numberOfParticles; i++)
+            {
+                var x = random.Next(0, width);
+                var y = random.Next(0, height);
+                bitmap.SetPixel(x, y, Color.Black);
+            }
+
+            return bitmap;
+        }
+
+        public static void FillBitmapWithColor(Color color, ref Bitmap bitmap)
+        {
+            using Graphics gfx = Graphics.FromImage(bitmap);
+            using SolidBrush brush = new SolidBrush(color);
+            gfx.FillRectangle(brush, 0, 0, bitmap.Width, bitmap.Height);
+        }
+
+        public static BitmapImage BitmapToImageSource(Bitmap bitmap)
+        {
+            using MemoryStream memory = new MemoryStream();
+            bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+            memory.Position = 0;
+            var bitmapimage = new BitmapImage();
+            bitmapimage.BeginInit();
+            bitmapimage.StreamSource = memory;
+            bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+            bitmapimage.EndInit();
+
+            return bitmapimage;
+        }
+
+        public static Bitmap BitmapImage2Bitmap(BitmapImage bitmapImage)
+        {
+            using MemoryStream outStream = new MemoryStream();
+            var enc = new BmpBitmapEncoder();
+            enc.Frames.Add(BitmapFrame.Create(bitmapImage));
+            enc.Save(outStream);
+            var bitmap = new Bitmap(outStream);
+
+            return new Bitmap(bitmap);
+        }
+    }
+}
--- Chladni-Plates/Chladni-Plates/MainWindow.xaml.cs
+++ Chladni-Plates/Chladni-Plates/MainWindow.xaml.cs
@@ -25,81 +25,8 @@ namespace Chladni_Plates
             var width = Convert.ToInt32(PixelBox.Width);
             var height = Convert.ToInt32(PixelBox.Height);
             var bitmap = new Bitmap(width, height);
-            FillBitmapWithColor(Color.LightGray, ref bitmap);
-            PixelBox.Source = BitmapToImageSource(bitmap);
-        }
-
-        private Bitmap RandomColorPixelBitmap(int width, int height)
-        {
-            var bitmap = new Bitmap(width, height);
-            var random = new Random();
-
-            //create random pixels
-            for (var y = 0; y < height; y++)
-            {
-                for (var x = 0; x < width; x++)
-                {
-                    //generate random ARGB
-                    var a = random.Next(256);
-                    var r = random.Next(256);
-                    var g = random.Next(256);
-                    var b = random.Next(256);
-
-                    //set
-                    bitmap.SetPixel(x, y, Color.FromArgb(a, r, g, b));
-                }
-            }
-
-            return bitmap;
-        }
-
-        private Bitmap RandomPixelsBitmap(int width, int height, int numberOfParticles)
-        {
-            var bitmap = new Bitmap(width, height);
-            FillBitmapWithColor(Color.LightGray, ref bitmap);
-
-            var random = new Random();
-
-            for (int i = 0; i < numberOfParticles; i++)
-            {
-                var x = random.Next(0, width);
-                var y = random.Next(0, height);
-                bitmap.SetPixel(x, y, Color.Black);
-            }
-
-            return bitmap;
-        }
-
-        private void FillBitmapWithColor(Color color, ref Bitmap bitmap)
-        {
-            using Graphics gfx = Graphics.FromImage(bitmap);
-            using SolidBrush brush = new SolidBrush(color);
-            gfx.FillRectangle(brush, 0, 0, bitmap.Width, bitmap.Height);
-        }
-
-        private BitmapImage BitmapToImageSource(Bitmap bitmap)
-        {
-            using MemoryStream memory = new MemoryStream();
-            bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
-            memory.Position = 0;
-            var bitmapimage = new BitmapImage();
-            bitmapimage.BeginInit();
-            bitmapimage.StreamSource = memory;
-            bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
-            bitmapimage.EndInit();
-
-            return bitmapimage;
-        }
-
-        private Bitmap BitmapImage2Bitmap(BitmapImage bitmapImage)
-        {
-            using MemoryStream outStream = new MemoryStream();
-            var enc = new BmpBitmapEncoder();
-            enc.Frames.Add(BitmapFrame.Create(bitmapImage));
-            enc.Save(outStream);
-            var bitmap = new Bitmap(outStream);
-
-            return new Bitmap(bitmap);
+            BitmapOperations.FillBitmapWithColor(Color.LightGray, ref bitmap);
+            PixelBox.Source = BitmapOperations.BitmapToImageSource(bitmap);
         }
 
         #region Handlers
@@ -109,13 +36,13 @@ namespace Chladni_Plates
             var height = Convert.ToInt32(PixelBox.Height);
             var numberOfParticles = Convert.ToInt32(ParticlesSlider.Value);
 
-            using var bitmap = RandomPixelsBitmap(width, height, numberOfParticles);
-            PixelBox.Source = BitmapToImageSource(bitmap);
+            using var bitmap = BitmapOperations.RandomPixelsBitmap(width, height, numberOfParticles);
+            PixelBox.Source = BitmapOperations.BitmapToImageSource(bitmap);
         }
 
         private void SaveToFile_Click(object sender, RoutedEventArgs e)
         {
-            using var bitmap = BitmapImage2Bitmap((BitmapImage)PixelBox.Source);
+            using var bitmap = BitmapOperations.BitmapImage2Bitmap((BitmapImage)PixelBox.Source);
             var path = Directory.GetCurrentDirectory();
             bitmap.Save(path + $"/Chladni-Plates-Frequency-{FrequencySlider.Value}Hz-Particles{ParticlesSlider.Value}.png");
         }
